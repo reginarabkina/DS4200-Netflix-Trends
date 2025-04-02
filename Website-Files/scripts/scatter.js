d112c1a1ec98bd1da417f18d4807fae5b060defd
Paulina Acosta <paulinaacosta1002@gmail.com>
2025-04-02 02:49:01 -0400

    updated visualization 2 - paulina

--- Website-Files/scripts/scatter.js
+++ Website-Files/scripts/scatter.js
@@ -1,45 +1,83 @@
-const scatterData =  d3.csv("../datasets/merged_data.json");
+d3.json("merged_data.json").then(function(data) {
+    // Parse data and group by release_year and type
+    let groupedData = d3.rollups(
+        data,
+        v => ({
+            num_releases: v.length,
+            avg_rating: d3.mean(v, d => +d.imdb_score)
+        }),
+        d => d.release_year,
+        d => d.type
+    );
 
-const width = 800;
-const height = 500;
-const margin = { top: 20, right: 30, bottom: 50, left: 50 };
+    let processedData = [];
+    groupedData.forEach(([year, types]) => {
+        types.forEach(([type, values]) => {
+            processedData.push({
+                release_year: year,
+                type: type,
+                num_releases: values.num_releases,
+                avg_rating: values.avg_rating
+            });
+        });
+    });
 
-const svg = d3.select("body")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    // Set up dimensions
+    const width = 800, height = 500, margin = { top: 50, right: 30, bottom: 50, left: 60 };
 
-const xScale = d3.scaleLinear()
-    .domain(d3.extent(scatterData, d => d.year))
-    .range([margin.left, width - margin.right]);
+    // Create scales
+    const xScale = d3.scaleBand()
+        .domain(processedData.map(d => d.release_year))
+        .range([margin.left, width - margin.right])
+        .padding(0.1);
+    
+    const yScale = d3.scaleLinear()
+        .domain([d3.min(processedData, d => d.avg_rating) - 0.5, d3.max(processedData, d => d.avg_rating) + 0.5])
+        .range([height - margin.bottom, margin.top]);
 
-const yScale = d3.scaleLinear()
-    .domain([0, 10])
-    .range([height - margin.bottom, margin.top]);
+    const sizeScale = d3.scaleLinear()
+        .domain([d3.min(processedData, d => d.num_releases), d3.max(processedData, d => d.num_releases)])
+        .range([10, 300]);
 
-svg.append("g")
-    .attr("transform", `translate(0,${height - margin.bottom})`)
-    .call(d3.axisBottom(xScale).tickFormat(d3.format("d")))
-    .append("text")
-    .attr("x", width / 2)
-    .attr("y", 40)
-    .attr("fill", "black")
-    .text("Year");
+    const colorScale = d3.scaleOrdinal()
+        .domain(["MOVIE", "SHOW"])
+        .range(["blue", "red"]);
 
-svg.append("g")
-    .attr("transform", `translate(${margin.left},0)`)
-    .call(d3.axisLeft(yScale))
-    .append("text")
-    .attr("x", -margin.left)
-    .attr("y", 10)
-    .attr("fill", "black")
-    .text("Average IMDb Rating");
+    // Create SVG container
+    const svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height);
 
-svg.selectAll("circle")
-    .data(scatterData)
-    .enter()
-    .append("circle")
-    .attr("cx", d => xScale(d.year))
-    .attr("cy", d => yScale(d.avg_score))
-    .attr("r", d => d.count * 2)
-    .attr("fill", "rgba(75, 192, 192, 0.6)");
+    // Add circles
+    svg.selectAll("circle")
+        .data(processedData)
+        .enter().append("circle")
+        .attr("cx", d => xScale(d.release_year) + xScale.bandwidth() / 2)
+        .attr("cy", d => yScale(d.avg_rating))
+        .attr("r", d => sizeScale(d.num_releases) / 10)
+        .attr("fill", d => colorScale(d.type))
+        .attr("opacity", 0.7);
+
+    // Add axes
+    const xAxis = d3.axisBottom(xScale).tickFormat(d => d);
+    const yAxis = d3.axisLeft(yScale);
+
+    svg.append("g")
+        .attr("transform", `translate(0,${height - margin.bottom})`)
+        .call(xAxis)
+        .selectAll("text")
+        .attr("transform", "rotate(-45)")
+        .style("text-anchor", "end");
+    
+    svg.append("g")
+        .attr("transform", `translate(${margin.left},0)`)
+        .call(yAxis);
+    
+    // Add title
+    svg.append("text")
+        .attr("x", width / 2)
+        .attr("y", margin.top / 2)
+        .attr("text-anchor", "middle")
+        .style("font-size", "16px")
+        .text("Number of Releases vs. Average Rating by Year (Movies & TV Shows)");
+});
